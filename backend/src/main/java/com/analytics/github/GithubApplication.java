673a7295e2ff3a40e280528c4cbbf6935ef9315a
eddy08 <bhardwajharsh08@gmail.com>
2023-05-18 23:46:37 +0530

    Updated Backend with Neo4j configuration

--- backend/src/main/java/com/analytics/github/GithubApplication.java
+++ backend/src/main/java/com/analytics/github/GithubApplication.java
@@ -2,8 +2,10 @@ package com.analytics.github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
+@EnableNeo4jRepositories
 public class GithubApplication {
 
 	public static void main(String[] args) {
